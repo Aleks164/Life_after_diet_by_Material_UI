c114886ccee1edbddccadde738bd9f1166ff4649
Levin Aleksey <68010219+Aleks164@users.noreply.github.com>
2022-08-23 09:52:20 +0400

    Create RecipeIdSceletonPage.tsx

---
+++ src/components/AfterAuthHomePage/RecipeByIdPage/RecipeIdSceletonPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Skeleton,Typography } from '@mui/material/';
+
+export const RecipeIdSceletonPage = ()=>(
+ <Skeleton width="100%">
+              <Typography>.</Typography>
+            </Skeleton>
+)
